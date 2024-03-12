c265cd492e591984c78f105cea4f799567e0f08a
Scott Kang <scottkang@microsoft.com>
2024-03-12 11:30:59 +0800

    shared AAD authContext

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,59 +1,58 @@
 import {
   AzureMap,
   AzureMapsProvider,
-  IAzureMapControls,
   IAzureMapOptions,
 } from "react-azure-maps";
-import { AuthenticationType, ControlOptions } from "azure-maps-control";
-import 'azure-maps-control/dist/atlas.min.css'
+import { AuthenticationType } from "azure-maps-control";
+import { Configuration, PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider, useMsal } from "@azure/msal-react";
 
-const option: IAzureMapOptions = {
-  authOptions: {
-    authType: AuthenticationType.subscriptionKey,
-    subscriptionKey: "", // Your subscription key
+import "azure-maps-control/dist/atlas.min.css";
+
+const Map: React.FC = () => {
+  const { instance, accounts, inProgress } = useMsal();
+  const isAuthenticated = accounts.length > 0 && inProgress === "none";
+
+  if (isAuthenticated) {
+    const option: IAzureMapOptions = {
+      authOptions: {
+        authType: AuthenticationType.aad,
+        clientId: "6b603e52-a0de-41df-bfa2-464e262fa984",
+        aadAppId: "761aa8c0-ab72-4dfe-8f47-8bd0718acae6",
+        authContext: instance,
+      },
+    };
+
+    return (
+      <div style={{ height: "600px" }}>
+        <AzureMapsProvider>
+          <AzureMap options={option} />
+        </AzureMapsProvider>
+      </div>
+    );
+  } else {
+    return <button onClick={() => instance.loginRedirect()}>Login</button>;
+  }
+};
+
+// MSAL configuration
+const configuration: Configuration = {
+  auth: {
+    clientId: "761aa8c0-ab72-4dfe-8f47-8bd0718acae6",
+    authority:
+      "https://login.microsoftonline.com/5122b9e0-9741-49fe-8990-94c45441f85d",
   },
 };
 
-const controls: IAzureMapControls[] = [
-  {
-    controlName: "StyleControl",
-    controlOptions: { mapStyles: "all" },
-    options: { position: "top-right" } as ControlOptions,
-  },
-  {
-    controlName: "ZoomControl",
-    options: { position: "top-right" } as ControlOptions,
-  },
-  {
-    controlName: "CompassControl",
-    controlOptions: { rotationDegreesDelta: 10, style: "dark" },
-    options: { position: "bottom-right" } as ControlOptions,
-  },
-  {
-    controlName: "PitchControl",
-    controlOptions: { pitchDegreesDelta: 5, style: "dark" },
-    options: { position: "bottom-right" } as ControlOptions,
-  },
-  {
-    controlName: "TrafficControl",
-    controlOptions: { incidents: true },
-    options: { position: "top-left" } as ControlOptions,
-  },
-  {
-    controlName: "TrafficLegendControl",
-    controlOptions: {},
-    options: { position: "bottom-left" } as ControlOptions,
-  },
-];
+// MSAL instance
+const pca = new PublicClientApplication(configuration);
 
-const Map: React.FC = () => {
+const MsalMap: React.FC = () => {
   return (
-    <AzureMapsProvider>
-      <div style={{ height: "600px" }}>
-        <AzureMap options={option} controls={controls} />
-      </div>
-    </AzureMapsProvider>
+    <MsalProvider instance={pca}>
+      <Map />
+    </MsalProvider>
   );
 };
 
-export default Map;
+export default MsalMap;
